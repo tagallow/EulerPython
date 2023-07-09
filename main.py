ffb9f4f231f212f3ec2d9817298d45c22410b684
Tom Galloway <tagallow@outlook.com>
2023-07-09 10:07:28 -0500

    added a timer

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import problem7 as currentProblem
+import time
 
 def main():
+    start_time = time.time()
     currentProblem.solve()
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
 
 if __name__ == "__main__":
     main()
